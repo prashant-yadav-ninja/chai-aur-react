01fa00d4d9e18ad3af2ee66219bcad17040b170f
Prashant Yadav <prashanty20720@gmail.com>
2023-11-23 22:05:03 +0530

    create my own react library and Jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./Chai";
 
 function App() {
+  const userName = "prashant";
   return (
     <>
       <Chai />
-      <h1>learn react from chai aur code using vite utitlty prashant yadav</h1>
+      <h1>
+        learn react from chai aur code using vite utitlty prashant yadav{" "}
+        {userName}{" "}
+      </h1>
     </>
   );
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// function Myapp() {
+//   return (
+//     <>
+//       <h1>Chai in myapp</h1>
+//       <h2>from direct function</h2>
+//     </>
+//   );
+// }
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to visit google",
+// };
+
+// const userName = 'prashant'
+
+// const reactElement = React.createElement(
+//   'a',
+//   {href:'https://google.com',target:'_blank'},
+//   'click me to go to google.com'
+//    userName
+// )
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank'>visit to google</a>
+// )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // anotherElement
+  // reactElement
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
