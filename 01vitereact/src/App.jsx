1bde01ca93b8907860707549dc2f705da559ce18
Prashant Yadav <prashanty20720@gmail.com>
2023-11-23 09:25:59 +0530

    understanding react flow and structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,12 @@
+import Chai from "./Chai";
+
 function App() {
-  return <h1>learn react from chai aur code prashant yadav</h1>;
+  return (
+    <>
+      <Chai />
+      <h1>learn react from chai aur code using vite utitlty prashant yadav</h1>
+    </>
+  );
 }
 
 export default App;
